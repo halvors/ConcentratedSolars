59650c6a69f1454ebf1e7ef7f9dd168119674e27
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-01-09 14:34:53 +0100

    Added solar panel.

--- src/main/java/org/halvors/ConcentratedSolars/common/block/BlockSolarPanel.java
+++ src/main/java/org/halvors/ConcentratedSolars/common/block/BlockSolarPanel.java
@@ -1,24 +1,30 @@
 package org.halvors.ConcentratedSolars.common.block;
 
-import java.util.Random;
-
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
 import org.halvors.ConcentratedSolars.ConcentratedSolars;
+import org.halvors.ConcentratedSolars.common.tileentity.TileEntitySolarPanel;
 
 public class BlockSolarPanel extends BlockContainer {
-	public BlockSolarPanel() {
+	public BlockSolarPanel(String name) {
 		super(Material.iron);
+
+		setBlockName(name);
 		setHardness(3.5F);
 		setResistance(8F);
+		//setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.3F, 1.0F);
 		setCreativeTab(ConcentratedSolars.tabConcentratedSolars);
+		setStepSound(soundTypeMetal);
+		
+		setBlockTextureName("ConcentratedSolars:blockSolarPanel");
 	}
 
 	@Override
 	public TileEntity createNewTileEntity(World world, int metadata) {
-		return null;
+		return new TileEntitySolarPanel();
 	}
 }
