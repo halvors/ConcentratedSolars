17fd2a9f7468ac5e87684552956292a25af46d2e
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-01-09 15:39:55 +0100

    Fixed creative tab locale.

--- src/main/java/org/halvors/ConcentratedSolars/CreativeTabConcentratedSolars.java
+++ src/main/java/org/halvors/ConcentratedSolars/CreativeTabConcentratedSolars.java
@@ -7,7 +7,7 @@ import net.minecraft.item.ItemStack;
 
 public class CreativeTabConcentratedSolars extends CreativeTabs {
 	public CreativeTabConcentratedSolars() {
-		super("tabConsentratedSolars");
+		super("tabConcentratedSolars");
 	}
 
 	@Override
