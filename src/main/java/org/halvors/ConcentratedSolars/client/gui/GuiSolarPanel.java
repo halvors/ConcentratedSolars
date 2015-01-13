3798fb784760a3138b41009369a2129f6ad9b615
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-01-13 16:15:27 +0100

    More gui work.

--- src/main/java/org/halvors/ConcentratedSolars/client/gui/GuiSolarPanel.java
+++ src/main/java/org/halvors/ConcentratedSolars/client/gui/GuiSolarPanel.java
@@ -3,6 +3,7 @@ package org.halvors.ConcentratedSolars.client.gui;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.StatCollector;
 
 import org.halvors.ConcentratedSolars.common.tileentity.TileEntitySolarPanel;
 import org.lwjgl.opengl.GL11;
@@ -14,10 +15,19 @@ public class GuiSolarPanel extends GuiContainer {
 	}
 
 	@Override
-	protected void drawGuiContainerBackgroundLayer(float p_146976_1_,
-			int p_146976_2_, int p_146976_3_) {
+    protected void drawGuiContainerForegroundLayer(int par1, int par2) {
+		//draw text and stuff here
+        //the parameters for drawString are: string, x, y, color
+		fontRendererObj.drawString("Solar Panel", 8, 6, 4210752);
+		
+        //draws "Inventory" or your regional equivalent
+		fontRendererObj.drawString(StatCollector.translateToLocal("container.inventory"), 8, ySize - 96 + 2, 4210752);
+    }
+	
+	@Override
+	protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3) {
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-		this.mc.renderEngine.bindTexture(new ResourceLocation("/gui/trap.png"));
+		mc.renderEngine.bindTexture(new ResourceLocation("ConcentratedSolars:gui/GuiBlank.png"));
 		int x = (width - xSize) / 2;
 		int y = (height - ySize) / 2;
 		drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
