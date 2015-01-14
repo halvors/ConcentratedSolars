24b9b8fe0abaaea3362351e487d055ddd2bff888
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-01-14 10:28:44 +0100

    Added Reference.

---
+++ src/main/java/org/halvors/ConcentratedSolars/Reference.java
@@ -0,0 +1,20 @@
+package org.halvors.ConcentratedSolars;
+
+public class Reference {
+	/** 
+	 * The official name of the mod.
+	 */
+	public static final String NAME = "Concentrated Solars";
+	public static final String MAJOR_VERSION = "@MAJOR@";
+	public static final String MINOR_VERSION = "@MINOR@";
+	public static final String REVISION_VERSION = "@REVIS@";
+	public static final String BUILD_VERSION = "@BUILD@";
+	public static final String VERSION = MAJOR_VERSION + "." + MINOR_VERSION+ "." + REVISION_VERSION;
+	
+	/**
+	 * Directory Information
+	 */
+	public static final String DOMAIN = "concentratedsolars";
+	public static final String PREFIX = DOMAIN + ":";
+	public static final String DIRECTORY = "/assets/" + DOMAIN + "/";
+}
