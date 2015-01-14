e43122a6bea255953073ba6afbe3fd8fdffddc76
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-01-14 10:29:03 +0100

    Textured solar panel.

--- src/main/java/org/halvors/ConcentratedSolars/common/block/BlockSolarPanel.java
+++ src/main/java/org/halvors/ConcentratedSolars/common/block/BlockSolarPanel.java
@@ -2,14 +2,23 @@ package org.halvors.ConcentratedSolars.common.block;
 
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
 import org.halvors.ConcentratedSolars.ConcentratedSolars;
+import org.halvors.ConcentratedSolars.Reference;
 import org.halvors.ConcentratedSolars.common.tileentity.TileEntitySolarPanel;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 public class BlockSolarPanel extends BlockContainer {
+	@SideOnly(Side.CLIENT)
+	public static IIcon topIcon, sideIcon, bottomIcon;
+	
 	public BlockSolarPanel(String name) {
 		super(Material.iron);
 
@@ -19,7 +28,6 @@ public class BlockSolarPanel extends BlockContainer {
 		setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.3F, 1.0F);
 		setCreativeTab(ConcentratedSolars.tabConcentratedSolars);
 		setStepSound(soundTypeMetal);
-		setBlockTextureName("ConcentratedSolars:blockSolarPanel");
 	}
 
 	@Override
@@ -28,6 +36,30 @@ public class BlockSolarPanel extends BlockContainer {
 	}
 	
 	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister iconRegister) {
+		topIcon = iconRegister.registerIcon(Reference.PREFIX + "blockSolarPanel_top");
+		sideIcon = iconRegister.registerIcon(Reference.PREFIX + "blockSolarPanel_side");
+		bottomIcon = iconRegister.registerIcon(Reference.PREFIX + "blockSolarPanel_bottom");
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public IIcon getIcon(int side, int meta) {
+		switch (side) {
+		case 1:
+			return topIcon;
+			
+		case 0:
+			return bottomIcon;
+			
+		default:
+			return sideIcon;
+		}
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
     public boolean isOpaqueCube() {
 		return false;
 	}
@@ -37,7 +69,7 @@ public class BlockSolarPanel extends BlockContainer {
         TileEntity tileEntity = world.getTileEntity(x, y, z);
         
         if (tileEntity == null || player.isSneaking()) {
-                return false;
+        	return false;
         }
         
 	    player.openGui(ConcentratedSolars.instance, 0, world, x, y, z);
