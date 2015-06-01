77c786cf1f759c7187b826480bab7fdd3d75afb6
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-06-01 21:03:47 +0200

    GUI cleanup.

--- src/main/java/org/halvors/ConcentratedSolars/gui/GuiSolarPanel.java
+++ src/main/java/org/halvors/ConcentratedSolars/gui/GuiSolarPanel.java
@@ -1,32 +1,63 @@
 package org.halvors.ConcentratedSolars.gui;
 
+import nova.core.gui.Background;
+import nova.core.gui.ComponentEvent;
+import nova.core.gui.Gui;
+import nova.core.gui.GuiEvent;
+import nova.core.gui.component.Button;
+import nova.core.gui.component.Container;
+import nova.core.gui.component.Label;
+import nova.core.gui.component.inventory.Slot;
+import nova.core.gui.layout.Anchor;
+import nova.core.gui.layout.FlowLayout;
+import nova.core.network.NetworkTarget;
+import nova.core.render.Color;
+import org.halvors.ConcentratedSolars.block.BlockSolarPanel;
+
 public class GuiSolarPanel extends Gui {
     public GuiSolarPanel() {
         super("guiSolarPanel");
 
-        add(new Button("testbutton2", "I'm EAST")
-                .setMaximumSize(Integer.MAX_VALUE, 120)
-                .onEvent((event, component) -> {
-                    System.out.println("Test button pressed! " + Side.get());
-                }, ActionEvent.class, Side.BOTH), Anchor.EAST);
+        Button button = new Button("testbutton2", "I'm EAST");
+        button.setMaximumSize(Integer.MAX_VALUE, 120);
 
-        add(new Button("testbutton3", "I'm CENTER"))
-                .add(new Container("test").setLayout(new FlowLayout())
-                        .add(new Slot("main", 0))
-                        .add(new Slot("main", 0))
-                        .add(new Slot("main", 0))
-                        .add(new Slot("main", 0)), Anchor.SOUTH)
-                .add(new Container("container").setLayout(new FlowLayout())
-                        .add(new Button("testbutton5", "I'm the FIRST Button and need lots of space"))
-                        .add(new Label("testlabel1", "I'm some label hanging around").setBackground(new Background(Color.white)))
-                        .add(new Button("testbutton7", "I'm THIRD"))
-                        .add(new Button("testbutton8", "I'm FOURTH")), Anchor.NORTH)
-                .onGuiEvent((event) -> {
-                    event.gui.addInventory("main", ((BlockSimpleTest)event.block.get()).inventory);
-                    System.out.println("Test GUI initialized! " + event.player.getDisplayName() + " " + event.position);
-                }, BindEvent.class)
-                .onGuiEvent((event) -> {
-                    System.out.println("Test GUI closed!");
-                }, UnBindEvent.class);
+        button.onEvent((event, component) -> {
+            System.out.println("Test button pressed! " + NetworkTarget.Side.get());
+        }, ComponentEvent.ActionEvent.class, NetworkTarget.Side.BOTH);
+
+        Button button1 = new Button("testbutton3", "I'm CENTER");
+
+        Container container = new Container("test");
+        container.setLayout(new FlowLayout());
+        container.add(new Slot("main", 0));
+        container.add(new Slot("main", 0));
+        container.add(new Slot("main", 0));
+        container.add(new Slot("main", 0));
+
+        Container container1 = new Container("container");
+        container1.setLayout(new FlowLayout());
+        container1.add(new Button("testbutton5", "I'm the FIRST Button and need lots of space"));
+
+        Label label = new Label("testlabel1", "I'm some label hanging around");
+        label.setBackground(new Background(Color.white));
+        container1.add(label);
+
+        container1.add(new Button("testbutton7", "I'm THIRD"));
+        container1.add(new Button("testbutton8", "I'm FOURTH"));
+
+        container1.onGuiEvent((event) -> {
+            event.gui.addInventory("main", ((BlockSolarPanel) event.block.get()).inventory);
+            System.out.println("Test GUI initialized! " + event.player.getDisplayName() + " " + event.position);
+        }, GuiEvent.BindEvent.class);
+
+        container1.onGuiEvent((event) -> {
+            System.out.println("Test GUI closed!");
+        }, GuiEvent.UnBindEvent.class);
+
+        // Add the components.
+        add(button, Anchor.EAST);
+        add(button1);
+        add(container, Anchor.SOUTH);
+        add(container1, Anchor.NORTH);
     }
 }
